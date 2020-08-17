97b94a9bef172415269db98d4e4a9fb0f8046940
Imrane Boucher <imrane.boucher@gmail.com>
2020-08-17 13:17:45 +0200

    added data cleaning start

---
+++ data_cleaning.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 17 12:19:39 2020
+
+@author: imran
+"""
+
+# Necessary librairies
+import pandas as pd
+
+# The first step of the project is the explore & clean the data, as well as 
+# understanding the various features available to later make our prediction 
+# I also want to forsee which features I can potentially later engeener as well
+# as noticing if the data needs mean normalization or feature 
+
+# import the data
+
+# the data is a ready to use dataset available on kaggle :
+# https://www.kaggle.com/bobbyscience/league-of-legends-diamond-ranked-games-10-min
+
+df = pd.read_csv('data/high_diamond_ranked_10min.csv')
+
+df.info()
+# there's no null values and only numerical data, the dataset seems already ready
+# for modelling 
+
+# the data contains 40 columns, 18 feature for each team, which means that some 
+# features are redundant. The big part of the job will therefore be feature
+# selection & feature engeeniring.
+
+# some features are useless in the prediction:
+    # - gameId 
+    # - blueTowersDestroyed (it is a pretty rare event before 10 min of game
+    # but can it have a powerful prediction effect? we need to analyze further
+    # this variable)
+    # redTotalGold & BlueTotalGold are well summarized by blueGoldDiff (we also don't need redGoldDiff)
+    # blueTotalExperience & redExperienceDiff can be replaced by BlueExperienceDiff 
+    # (we also don't need redExperienceDiff)
+    # we don't need RedFirstBlood (only BlueFirstBlood)
+    # ...
+    
+# we can engeener some features by replacing for instance Red/BlueTtotalJungleMinionsKilled
+# by blueDiffJungleMinionsKilled
+
